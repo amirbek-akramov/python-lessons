7409ba10e43b5ba8605133794c74e436a709eaf4
Akramov Amirbek <amirbekcoder@gmail.com>
2023-02-17 10:35:54 +0500

    New files added
    New files: 5,6,7,8,9,10,11,12,13 - lessons.py

---
+++ 13 - lesson/13 - GitHub.py
@@ -0,0 +1,47 @@
+"""
+# 13-dars: GitHub
+sana: 16/02/2023
+"""
+
+
+"""
+!!!
+
+
+Tajribali dasturchi, tajribasiz dasturchidan bir necha barobar qadrliroq. 
+Dasturchi sifatida qilgan dasturlaringiz, 
+qatnashgan loyihlaringiz haqida so'ralishingiz mumkin 
+(masalan ish qidirish jarayonida). 
+Tajribani ko'rsatishning eng qulay va oson usuli bu qilgan ishlaringizning 
+onlayn portfoliosini yaratish. 
+"""
+
+
+"""
+| 'I'
+| 
+| Sifatli va loyihalarga boy portfolio 
+| Universitet diplomi va turli sertifikatlardan ham qadrliroq. 
+| 
+| 
+"""
+
+
+"""
+GitHub nima?
+
+
+GitHub - dasturlarni saqlash, 
+ulashish va nazorat qilish uchun mo'ljallangan onlayn xosting hizmati. 
+GitHub dasturchilarga biror loyiha ustida bir-biriga halaqit qilmagan holda 
+jamoa bo'lib ishlash, 
+dasturning turli versiyalarini nazorat qilib borish imkonini beradi 
+(lekin biz bu haqida keyingi darslarimizning birida to'xtalamiz).
+ 
+
+Bu darsda esa biz qanday qilib GitHubga loyihalarimizni yuklash, 
+ulashish va boshqalarning loyihalarini ko'chirib olish haqida gaplashamiz.
+
+"""
+
+
