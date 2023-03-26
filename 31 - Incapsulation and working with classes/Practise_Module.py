bf8ef145a5467f00cbd6b02fe04f64ccb9f10409
Akramov Amirbek <125534574+amirbek-akramov@users.noreply.github.com>
2023-03-26 14:46:48 +0500

    26/03/2023

---
+++ 31 - Incapsulation and working with classes/Practise_Module.py
@@ -0,0 +1,105 @@
+"Practise"
+
+class Person:
+    __person_amount = 0
+    """Create a person"""
+    def __init__(self, name, surname, birth_year, passport, __health):
+        self.name = name
+        self.surname = surname
+        self.birth_year = birth_year
+        self.passport = passport
+        self.__health = __health
+        Person.__person_amount += 1
+    
+    @classmethod
+    def get_person_amount(cls):
+        return cls.__person_amount
+
+    def get_about_health(self):
+        return self.__health
+
+    def set_health(self, __health):
+        self.__health = __health
+
+    def get_age(self, year = 2023):
+        return year - self.birth_year
+            
+    def get_info(self):
+        """Returns information about person"""
+        return f"Person's information: \n Name: {self.name.title()} \n Surname: {self.surname.title()} \n Age: {self.get_age()}"
+    
+    def get_birth_year(self):
+        """Birth year of person"""
+        return self.birth_year
+    
+    def get_passport(self):
+        password = input("Enter the password: ")
+        return self.passport if password == "see_passport1234" else "Invalid password"
+
+"""_________________________________________________________________________"""
+
+class Location:
+    """Location"""
+    def __init__(self, house, street, town, country):
+        self.house = house
+        self.street = street
+        self.town = town
+        self.country = country
+        
+    def get_location(self):
+        """Returns the location"""
+        return f"Location: \n  House: {self.house} \n  Street: {self.street} \n  Town: {self.town} \n  Country: {self.country}"
+
+
+"""________________________________Succession_______________________________"""
+
+
+
+
+
+class Student(Person):
+    __student_amount = 0
+    def __init__(self, name, surname, birth_year, passport, __health, __hobby, ID, location):
+        super().__init__(name, surname, birth_year, passport, __health)
+        self.ID = ID
+        self.level = 1
+        self.location = location
+        self.__hobby = __hobby
+        Student.__student_amount += 1
+        
+    @classmethod
+    def get_hobby(self):
+        """Student's hobby"""
+        return self.__hobby
+    
+    def set_hobby(self,hobby):
+        """Changing the hobby of student"""
+        self.__hobby = hobby
+    
+    def set_level(self, level):
+        self.level = level
+        return "Level successfully added"
+    
+    def get_level(self):
+        """Returns level of student"""
+        return self.level    
+        
+    def get_id(self):
+        """Returns id of student"""
+        return self.ID
+
+    def get_info(self):
+        """Returns information about student"""
+        return f"Student's information: \n Name: {self.name.title()} \n Surname: {self.surname.title()} \n Age: {self.get_age()} \n Id: {self.ID} \n Level: {self.level} \n Hobby: {self.__hobby} \n\n {self.location.get_location()}"
+    
+    
+    
+person1 = Person("Jack", "Michaelson", 1985, "FFBB3322213", "nice")
+
+student1_location = Location(99, "Karvon", "Umid", "Uzbekistan")
+student1 = Student("Mike", "Jackson", 1982, "FFBB9994342", "normal", "Playing tennis", 34, student1_location)
+
+tab_enter = "\n\n\n\n"
+print(person1.get_info(), end = tab_enter)
+print(student1_location.get_location(), end = tab_enter)
+print(student1.get_info(), end = tab_enter)
